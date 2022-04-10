7be1e9eaf73f2c9f047c5e9baabfdfc3c8a9d92c
Eric Shumway <ericshumway2@mail.weber.edu>
2022-04-09 19:12:59 -0600

    slight modifications to generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -54,10 +54,6 @@ const renderLicense = license => {
   `;
 };
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-
-
 // TODO: Create a function to generate markdown for README
 module.exports = data => {
   console.log(data);
